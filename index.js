de3265d5ce5b5303bd9a34b2bfacb9d9a036d707
lim-jiahao <lim.jiahaoo@gmail.com>
2021-10-28 10:56:39 +0800

    changes in new branch

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
-const myName = process.argv[2];
-console.log(`hello ${myName.toUpperCase()}! How are you?`);
+import { writeFile } from 'fs';
+let name = process.argv[2];
+writeFile('data.json', name, (err) => console.error('err ->', err));
